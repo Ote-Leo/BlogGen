7ecf9e48e5f878dfa7f0fa4b7f236b4551a6253f
Ote Leo <ote.leo73@gmail.com>
2023-06-22 21:10:09 +0300

    Updating Convert for new EDSL

--- src/HsBlog/Convert.hs
+++ src/HsBlog/Convert.hs
@@ -8,8 +8,8 @@ convert title = Html.html_ title . foldMap convertStructure
 
 convertStructure :: Markup.Structure -> Html.Structure
 convertStructure struct = case struct of
-  Markup.Heading lvl txt -> Html.h_ lvl txt
-  Markup.Paragraph txt -> Html.p_ txt
-  Markup.OrderedList lst -> Html.ol_ . map Html.p_ $ lst
-  Markup.UnorderedList lst -> Html.ul_ . map Html.p_ $ lst
+  Markup.Heading lvl txt -> Html.h_ lvl $ Html.txt_ txt
+  Markup.Paragraph txt -> Html.p_ $ Html.txt_ txt
+  Markup.OrderedList lst -> Html.ol_ . map (Html.p_ . Html.txt_) $ lst
+  Markup.UnorderedList lst -> Html.ul_ . map (Html.p_ . Html.txt_) $ lst
   Markup.CodeBlock lst -> Html.code_ . unlines $ lst
