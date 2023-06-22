3ca9f7ce9c837db139c9131bfaae33c2f60207b7
Ote Leo <ote.leo73@gmail.com>
2023-06-22 23:36:46 +0300

    Adding Page Index Builder

--- src/HsBlog/Convert.hs
+++ src/HsBlog/Convert.hs
@@ -13,3 +13,20 @@ convertStructure struct = case struct of
   Markup.OrderedList lst -> Html.ol_ . map (Html.p_ . Html.txt_) $ lst
   Markup.UnorderedList lst -> Html.ul_ . map (Html.p_ . Html.txt_) $ lst
   Markup.CodeBlock lst -> Html.code_ . unlines $ lst
+
+buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
+buildIndex files = Html.html_ "Blog" (title <> listings <> previews)
+  where
+    title = Html.h_ 1 . Html.link_ "index.html" . Html.txt_ $ "Blog"
+    listings = Html.h_ 2 . Html.txt_ $ "Posts"
+    previews = mconcat . preview $ files
+
+preview :: [(FilePath, [Markup.Structure])] -> [Html.Structure]
+preview = map mkLink
+  where
+    mkLink (file, doc) = case doc of
+      Markup.Heading 1 heading : article ->
+        (Html.h_ 3 . Html.link_ file . Html.txt_ $ heading)
+          <> (foldMap convertStructure . take 3 $ article)
+          <> (Html.p_ . Html.link_ file . Html.txt_ $ "...")
+      _ -> Html.h_ 3 . Html.link_ file . Html.txt_ $ file
